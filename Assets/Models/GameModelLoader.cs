65b2d3fb703887b5a052b1d5387a39cd3c68c968
amir <amir@gmail.com>
2017-04-10 12:34:35 +0430

    Model added

---
+++ Assets/Models/GameModelLoader.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Xml;
+public class GameModelLoader{
+
+    //Get data from xml file
+    public static int row,col,square_size,wall_density;
+    static GameModelLoader() { 
+        XmlDocument doc = new XmlDocument();
+        doc.Load("Assets\\Models\\GameModel.xml");
+
+        //setting tags
+        XmlNodeList elemList = doc.GetElementsByTagName("row");
+        foreach(XmlNode x in elemList)
+        {
+            row =XmlConvert.ToInt32( x.InnerText);
+        }
+        elemList = doc.GetElementsByTagName("col");
+
+        foreach (XmlNode x in elemList)
+        {
+            col = XmlConvert.ToInt32(x.InnerText);
+        }
+        elemList = doc.GetElementsByTagName("squaresize");
+
+        foreach (XmlNode x in elemList)
+        {
+            square_size = XmlConvert.ToInt32(x.InnerText);
+        }
+        elemList = doc.GetElementsByTagName("wallDensity");
+       
+        foreach(XmlNode x in elemList)
+        {
+            wall_density =XmlConvert.ToInt32( x.InnerText);
+        }
+       
+
+    }
+}
