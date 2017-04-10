59410a090d80ab3353ff7a37215bbeab47d12ee2
amir <amir@gmail.com>
2017-04-10 13:30:16 +0430

    pacman obj added to view

--- Assets/View/MapView.cs
+++ Assets/View/MapView.cs
@@ -22,30 +22,19 @@ public class MapView : MonoBehaviour {
         initiate();
         
         obj = new GameObject[row,col];
-        char x = '0';
         pacman = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        bool e = true;
+        pacman.transform.position = new Vector3(GameData.pac_row, GameData.pac_col, 0);
+        pacman.GetComponent<Renderer>().material.color = Color.red;
         for (int i = 0; i < row; i++)
         {
             for (int j = 0; j < col; j++) {
-                if( GameData.map[i,j]==1 && e)
-                {
-                    e = false;
-                    pacman.transform.position = new Vector3(i, j, 0);
-                    pacman.GetComponent<Renderer>().material.color = Color.red;
-                  
-                }
+              
                 if (GameData.map[i, j] == -1 )
                 {
                     obj[i, j] = GameObject.CreatePrimitive(PrimitiveType.Cube);
 
                     obj[i, j].transform.position = new Vector3(i, j, 0);
                     obj[i, j].GetComponent<Renderer>().material.color = Color.blue;
-                    
-
-
-                    obj[i, j].name = "cube" + x;
-                    x = (char)((int)x + 1);
                 }
             }
 
