52d9cf19e40f500e2810cb405e3d1bfed77edfbd
amir <amir@gmail.com>
2017-04-10 15:07:43 +0430

    pacman moving with key

---
+++ Assets/Controller/Pacman.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+public class Pacman : MonoBehaviour {
+    public int pac_row, pac_col;
+    public float x, y;
+    public Pacman()
+    {
+        RandomPacman.randomPlace();
+        pac_row = GameData.pac_row;
+        pac_col = GameData.pac_row;
+        x = pac_row;
+        y = pac_col;
+
+    }
+    public void moveRight()
+    {   
+         x += 0.1f;
+         GameData.pacx = x;
+    }
+}
---
+++ Assets/View/move.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+    Pacman p;
+	// Use this for initialization
+	void Start () {
+        p = new Pacman();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A))
+        {
+            float time = Time.time;
+            p.moveRight();
+
+        }
+        
+        /*for(int i = 0; i < 100; i++)
+        {
+            if(Time.time == time + i)
+            {
+                p.moveRight();
+            }
+        }*/
+	}
+}
